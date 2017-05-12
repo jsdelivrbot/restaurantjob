35accb1612f9e7b9c04db1c87da7ccfc4f9200c7
Peng Wu <peng116233402@gmail.com>
2017-05-11 21:34:29 -0600

    add some Jobtitle components

---
+++ src/components/FindJobFilter.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import JobtitleDropdown from "./JobtitleDropdown";
+
+export default class FindJobFilter extends Component {
+  render() {
+    return (
+      <JobtitleDropdown />
+    )
+  }
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import FindJobFilter from "./FindJobFilter";
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <FindJobFilter />
+      </div>
     );
   }
 }
